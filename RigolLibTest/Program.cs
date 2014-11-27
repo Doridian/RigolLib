d01a620d311e0d8377eed5eea907fd8f122754a2
Mark Dietzer <mriq91@gmail.com>
2014-11-27 12:38:17 +0100

    Seperate out silly local tests to prevent commitspam

--- RigolLibTest/Program.cs
+++ RigolLibTest/Program.cs
@@ -1,33 +1,11 @@
-﻿using RigolLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
+﻿
 namespace RigolScopeLibTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Oscilloscope oscilloscope = DeviceEnumerator.FindScopes().First();
-            
-            Waveform waveform = oscilloscope.GetWaveform(1, true);
-
-            Console.Out.WriteLine(DateTime.UtcNow);
-            long starttime = DateTime.Now.ToFileTimeUtc();
-            for (int i = 0; i < 1; i++)
-                oscilloscope.QueryWaveform();
-            double time = DateTime.Now.ToFileTimeUtc() - starttime;
-            Console.Out.WriteLine(DateTime.UtcNow);
-
-            Console.Out.WriteLine((time * 1.0e-9) * 100);
-
-            /*foreach(KeyValuePair<double, double> point in waveform.GetPoints())
-            {
-                Console.Out.WriteLine(point.Key + waveform.xunit + " => " + point.Value + waveform.yunit);
-            }*/
-
-            Console.In.ReadLine();
+            LocalTests.Run();
         }
     }
 }
